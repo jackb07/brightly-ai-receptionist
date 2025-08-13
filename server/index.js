65f22abde99950dbd5397246624b471cfedba266
jackb07 <bogjack07@gmail.com>
2025-08-13 17:04:54 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -42,4 +42,8 @@ app.post("/twilio/voice-bridge", (req, res) => {
   res.type("text/xml").send(twiml.toString());
 });
 
-app.listen(3000, () => console.log("Brightly AI server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
